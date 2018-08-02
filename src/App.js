7c554399444e5543d8a91a1fbbdc9abd32654562
Balazs Borzovan <balazs.borzovan@gmail.com>
2018-08-02 19:54:34 +0200

    almost final

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Map from './Map.js';
 import Sidebar from './Sidebar.js';
 
 class App extends Component {
-	
+
+// declaring initial state needed for the application
 state = {
 	locations: [
 	{title:'Indigo', location: {lat: 47.4918275, lng: 19.0391501}},
@@ -16,22 +17,37 @@ state = {
 	{title:'Hongkong Restaurant', location: {lat: 47.534545, lng: 19.082193}}
 	],
 	currentLocations: [
-		{title:'Indigo', location: {lat: 47.4918275, lng: 19.0391501}},
-		{title:'Rapaz', location: {lat: 47.4977522, lng: 19.070250999999985}},
-		{title:'Bors Gastrobar', location: {lat: 47.4967267, lng: 19.063548699999956}},
-		{title:'Buda Gourmet Bistro', location: {lat: 47.528095, lng: 19.037538}},
-		{title:'Biwako Ramen', location: {lat: 47.507306, lng: 19.062940}},
-		{title:'Hongkong Restaurant', location: {lat: 47.534545, lng: 19.082193}}
+	{title:'Indigo', location: {lat: 47.4918275, lng: 19.0391501}},
+	{title:'Rapaz', location: {lat: 47.4977522, lng: 19.070250999999985}},
+	{title:'Bors Gastrobar', location: {lat: 47.4967267, lng: 19.063548699999956}},
+	{title:'Buda Gourmet Bistro', location: {lat: 47.528095, lng: 19.037538}},
+	{title:'Biwako Ramen', location: {lat: 47.507306, lng: 19.062940}},
+	{title:'Hongkong Restaurant', location: {lat: 47.534545, lng: 19.082193}}
 	],
 	query: '',
 	lastOpenedWindow: '',
 	lastMarkerClicked: ''
 }
 
+// fetches fake users for the app and checks if the arrive
 componentDidMount() {
-  fetch("https://randomuser.me/api/?results=6").then(res => res.json()).then(myJson => this.setState({users: myJson.results}))
-}
+    fetch("https://randomuser.me/api/?results=6")
+		.then(res => {
+        if(res.ok) {
+			return res.json()
+        } else {
+			throw new Error(res.statusText)
+        }
+        })
+      .then(myJson => {
+		this.setState({users: myJson.results})
+        })
+        .catch(err => {
+			this.setState({error: err.toString()})
+        })
+}	
 
+// gets which marker was clicked in order to handle Animation
 getLastMarkerClicked = (marker) => {	
 	if (this.state.getLastMarkerClicked !== marker && this.state.lastMarkerClicked){
 		this.state.lastMarkerClicked.setAnimation(-1)
@@ -39,48 +55,59 @@ getLastMarkerClicked = (marker) => {
 	this.setState({lastMarkerClicked: marker})
 }
 
+// gets the infowindow so it can be closed when the user searches
 getLastOpenedWindow = (infowindow) => {
-infowindow.marker = null
-this.setState({lastOpenedWindow: infowindow})
+	infowindow.marker = null
+	this.setState({lastOpenedWindow: infowindow})
 }
 
+// searches for the appropriate locations in the list
 queryUpdate = (query) => {
-  this.setState({query: query})
+	
+	// updateing the state
+    this.setState({query: query})
 	this.setState({currentLocations : []})
+	
+	// clearing lefover animation
 	this.state.lastMarkerClicked.setAnimation(-1)
   
-   if (query) {
+    // updating which locations need to be listed
+    if (query) {
 	    this.state.locations.forEach((ele) => {
 			if (ele.title.toLowerCase().includes(query.toLowerCase())) {
-			  this.setState((prevState) => ({currentLocations: [...prevState.currentLocations, ele]}))
+			    this.setState((prevState) => ({currentLocations: [...prevState.currentLocations, ele]}))
 			}
-	})   
-   } else {
+	    })   
+    } else {
 	    this.setState({currentLocations : this.state.locations})
-	 }
-	 if (this.state.lastOpenedWindow !== ''){
+	}
+	// shutting down that pesky window
+	if (this.state.lastOpenedWindow !== ''){
 		this.state.lastOpenedWindow.close()
-	 }
+	}
 }
-	
+
 render() {
     return (
 	<div className="App">	
 		<header>
 			<h1>Budapest Eats</h1>
 		</header>
-
-		<div className="flex-container">
-			<Sidebar queryUpdate={this.queryUpdate} currentLocations={this.state.currentLocations} users={this.state.users}/>
-			<Map google={this.props.google} currentLocations={this.state.currentLocations}
-			getLastOpenedWindow={this.getLastOpenedWindow} getLastMarkerClicked={this.getLastMarkerClicked} users={this.state.users}/>
-		</div>
+		{this.state.error ? 
+            <div className="map-error">
+                <p>An error has occurred loading resources, please try again!</p>
+            <div>Error is {this.state.error}</div>
+            </div> :	
+		    <div className="flex-container">
+			    <Sidebar queryUpdate={this.queryUpdate} currentLocations={this.state.currentLocations} users={this.state.users}/>
+			    <Map google={this.props.google} currentLocations={this.state.currentLocations} getLastOpenedWindow={this.getLastOpenedWindow} getLastMarkerClicked={this.getLastMarkerClicked} users={this.state.users}/>
+		    </div>}
 		<footer>
 			<h3>© Bubicica designs and webz</h3>
 		</footer>
     </div>
     );
-  }
+}
 }
 
 export default GoogleApiWrapper({
--- src/Map.js
+++ src/Map.js
@@ -4,99 +4,119 @@ class Map extends Component {
 
 // define initial state
 state = {
-  markers: [],
-  infowindow: new this.props.google.maps.InfoWindow(),
-  mapRef: React.createRef()
+    markers: [],
+    infowindow: new this.props.google.maps.InfoWindow(),
+    mapRef: React.createRef(),
+    errorWithMap: ''
 }
 
+// load dat map and activate sidebar functions
 componentDidMount() {
-  this.loadMap()
-  this.onLocationClick()
+    this.loadMap()
+    this.onLocationClick()
 }
 
+// set marker visibility whenever there is an update
 componentDidUpdate(){
-  this.state.markers.forEach((marker) => {
-    marker.setVisible(false)
-    this.props.currentLocations.forEach((location) => {
-      if (marker.title.toLowerCase() === location.title.toLowerCase()) {
-        marker.setVisible(true)
-      }
-    }
-  )
-  })
+    this.state.markers.forEach((marker) => {
+        marker.setVisible(false)
+        this.props.currentLocations.forEach((location) => {
+            if (marker.title.toLowerCase() === location.title.toLowerCase()) {
+                marker.setVisible(true)
+            }
+        })
+    })
 }
 
+// defines how the map is set up initially and throws errors
 loadMap() {
-  if (this.props && this.props.google) {
-    const node = this.state.mapRef.current
+    if (this.props && this.props.google) {
+        const node = this.state.mapRef.current
+        const mapConfig = {
+            center: {lat: 47.498, lng: 19.05455},
+            zoom: 12,
+        }
 
-    const mapConfig = {
-      center: {lat: 47.498, lng: 19.05455},
-      zoom: 12,
+        this.map = new this.props.google.maps.Map(node, mapConfig)
+        this.addMarkers()
+        } else {
+	    this.setState({errorWithMap: "Error while loading your map, sorry :("})
+        }
     }
 
-    this.map = new this.props.google.maps.Map(node, mapConfig)
-    this.addMarkers()
-  }
-}
-
+// adds Markers to the Map with associated Infowindows
 addMarkers = () => {
-  this.props.currentLocations.forEach((location, index) => {
-    const marker = new this.props.google.maps.Marker({
-      position: {lat: location.location.lat, lng: location.location.lng},
-      map: this.map,
-      title: location.title,
-    })
+    this.props.currentLocations.forEach((location, index) => {
+        const marker = new this.props.google.maps.Marker({
+            position: {lat: location.location.lat, lng: location.location.lng},
+            map: this.map,
+            title: location.title,
+        })
+		
+        marker.addListener('click', () => {
+            this.fillInfoWindow(marker, this.state.infowindow, this.props.users[index])
+        })
 
-   marker.addListener('click', () => {
-      this.populateInfoWindow(marker, this.state.infowindow, this.props.users[index])
+        this.setState((state) => ({
+            markers: [...state.markers, marker]
+        }))
     })
-
-    this.setState((state) => ({
-      markers: [...state.markers, marker]
-    }))
-  })
 }
 
-populateInfoWindow = (marker, infowindow, user) => {
-  // Check to make sure the infowindow is not already opened on this marker.
-  if (infowindow.marker !== marker) {
-    infowindow.marker = marker
-    infowindow.setContent("<h4>" + marker.title + "</h4><p><img src='" + user.picture.thumbnail + "' alt='user profile picture' class='user-thumbnail'/></p><p class='like-text'>" + user.name.first[0].toUpperCase() + user.name.first.substring(1) + " " + user.name.last[0].toUpperCase() + user.name.last.substring(1) +" recently liked this place!</p>")
+// Fill up the infowindows with useful information - and load userdata async
+fillInfoWindow = (marker, infowindow, user) => {
+    if (infowindow.marker !== marker) {
+        infowindow.marker = marker
+	
+	// create the content here if there are users in the props
+	if (this.props.users){
+		infowindow.setContent("<h4>" + marker.title + "</h4><p><img src='" + user.picture.thumbnail + "' alt='user profile picture' class='user-thumbnail'/></p><p class='like-text'>" + user.name.first[0].toUpperCase() + user.name.first.substring(1) + " " + user.name.last[0].toUpperCase() + user.name.last.substring(1) +" recently liked this place!</p>")	
+	}
+    
+	// pop-open that window
     infowindow.open(this.map, marker)
-    // Make sure the marker property is cleared if the infowindow is closed.
+	
+    // Getting rid of the marker property of the infowindow
     infowindow.addListener('closeclick', function () {
-      infowindow.marker = null
+        infowindow.marker = null
     })
+	
+	// updating the state with the current infowindow
     this.props.getLastOpenedWindow(infowindow)
 
     // animating markers
     this.props.getLastMarkerClicked(marker)
     marker.setAnimation(this.props.google.maps.Animation.BOUNCE)
-    
   }
 }
 
+// pop-up appropriate infowindow if user clicks on the list
 onLocationClick = () => {
     const that = this
     const displayInfowindow = (event) => {
-      const markerInd = this.state.markers.findIndex(marker => marker.title.toLowerCase() === event.target.innerText.toLowerCase())
-      that.populateInfoWindow(this.state.markers[markerInd], this.state.infowindow, this.props.users[markerInd])
+        const markerInd = this.state.markers.findIndex(marker => marker.title.toLowerCase() === event.target.innerText.toLowerCase())
+	    
+		// feed info to infowindow if user is availble
+	    if (this.props.users){
+		that.fillInfoWindow(this.state.markers[markerInd], this.state.infowindow, this.props.users[markerInd])  
+	    }
     }
 	
+	// get to know which LI was clicked
     document.querySelector('.location-list').addEventListener('click', function (event) {
-      if (event.target && event.target.nodeName === "LI") {
-        displayInfowindow(event)
-      }
+        if (event.target && event.target.nodeName === "LI") {
+            displayInfowindow(event)
+        }
     })
-  }
+}
 
 render() {
    return(
       <div className="map" role="application" ref={this.state.mapRef}>
-
+        {this.state.errorWithMap && <div className="map-error">{this.state.errorWithMap}</div>}
       </div>
    );
    }
 };
+
 export default Map;
